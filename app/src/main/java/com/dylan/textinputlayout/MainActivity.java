fa941338809a28d812365ac136bf9729adb9a94d
VincentWYJ <wyjxjm@126.com>
2017-04-28 16:12:02 +0800

    Add password edittext

--- app/src/main/java/com/dylan/textinputlayout/MainActivity.java
+++ app/src/main/java/com/dylan/textinputlayout/MainActivity.java
@@ -7,11 +7,17 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
 
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(
+            "\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*");
     private TextInputLayout mTILUsername;
-    private TextInputLayout mTILEmail;
     private EditText mETUsername;
+    private TextInputLayout mTILEmail;
     private EditText mETEmail;
+    private TextInputLayout mTILPassword;
+    private EditText mETPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +26,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTILUsername = (TextInputLayout) findViewById(R.id.til_username);
-        mTILEmail = (TextInputLayout) findViewById(R.id.til_email);
         mETUsername = (EditText) findViewById(R.id.et_username);
+        mTILEmail = (TextInputLayout) findViewById(R.id.til_email);
         mETEmail = (EditText) findViewById(R.id.et_email);
+        mTILPassword = (TextInputLayout) findViewById(R.id.til_password);
+        mETPassword = (EditText) findViewById(R.id.et_password);
 
         mETUsername.addTextChangedListener(new TextWatcher() {
             @Override
@@ -30,9 +38,9 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (s.length() > 5) {
+                if (s.length() > 10) {
                     mTILUsername.setErrorEnabled(true);
-                    mTILUsername.setError("Max length is 5.");
+                    mTILUsername.setError("Username max length is 10.");
                 } else {
                     mTILUsername.setErrorEnabled(false);
                 }
@@ -48,11 +56,29 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (s.length() > 5) {
+                if (!EMAIL_PATTERN.matcher(s.toString().trim()).matches()) {
                     mTILEmail.setErrorEnabled(true);
-                    mTILEmail.setError("Max length is 5.");
+                    mTILEmail.setError("Please input correct email.");
                 } else {
                     mTILEmail.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        mETPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() < 6) {
+                    mTILPassword.setErrorEnabled(true);
+                    mTILPassword.setError("Password min length is 6.");
+                } else {
+                    mTILPassword.setErrorEnabled(false);
                 }
             }
 
